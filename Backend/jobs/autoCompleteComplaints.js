8f5939314a8cf7f4bc5063d6f527ed6fe223e2ad
Gokul <gokulkrishnagkr123@gmail.com>
2026-01-18 22:29:37 +0530

    Fix complaint model import casing for Linux deployment

--- Backend/jobs/autoCompleteComplaints.js
+++ Backend/jobs/autoCompleteComplaints.js
@@ -1,4 +1,4 @@
-const Complaint = require("../models/Complaint");
+const Complaint = require("../models/complaint");
 const Notification = require("../models/Notification");
 
 const HOURS = (h) => h * 60 * 60 * 1000;
